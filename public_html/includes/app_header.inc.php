36c6864057b5e37c586d77b8ea0a1e96fb6ba402
xmexpi <32973947+sedinmahmutovic@users.noreply.github.com>
2021-04-09 12:22:44 -0400

    Small update on installation process

--- public_html/includes/app_header.inc.php
+++ public_html/includes/app_header.inc.php
@@ -1,31 +1,26 @@
 <?php
-  define('PLATFORM_NAME', 'xMexpi');
-  define('PLATFORM_VERSION', '1.0.1');
+define('PLATFORM_NAME', 'xMexpi');
+define('PLATFORM_VERSION', '1.0.1');
 
-  if (!file_exists(__DIR__ . '/config.inc.php')) {
-    header('Location: ./install/');
-    exit;
-  }
-  if (!file_exists(__DIR__ . '/license.inc.php')) {
-    echo "License Error";;
-    exit;
-  }
+if (!file_exists(__DIR__ . '/config.inc.php')) {
+  header('Location: ./install/');
+  exit;
+}
+if (!file_exists(__DIR__ . '/license.inc.php')) {
+  echo "License Error";;
+  exit;
+}
 // Start redirecting output to the output buffer
-  ob_start();
+ob_start();
 
 // Get config
-  require_once __DIR__ . '/license.inc.php';
+require_once __DIR__ . '/license.inc.php';
 // Jump-start some library modules
-  class_exists('compression');
-  class_exists('notices');
-  class_exists('stats');
-  if (file_get_contents('php://input')) {
-    class_exists('form');
-  }
+class_exists('compression');
+class_exists('notices');
+class_exists('stats');
+if (file_get_contents('php://input')) {
+  class_exists('form');
+}
 // Run operations before capture
-  event::fire('before_capture');
-  
-
-
-
-  echo (LICENSE_KEY);
+event::fire('before_capture');
--- public_html/includes/license.inc.php
+++ public_html/includes/license.inc.php
@@ -2,12 +2,9 @@
 ######################################################################
 ## License File Checker #############################################
 ######################################################################
-if (!settings::get('site_configuration')) {
-  echo settings::get('site_configuration');
-}
-
 // Get config
 require_once __DIR__ . '/config.inc.php';
+
 // Virtual Modifications System
 require_once __DIR__ . '/library/lib_vmod.inc.php';
 vmod::init(); // Requires hard initialization as autoloader comes later
@@ -24,3 +21,18 @@ if (is_file(FS_DIR_APP . 'vendor/autoload.php')) {
 }
 // Set error handler
 require_once vmod::check(FS_DIR_APP . 'includes/error_handler.inc.php');
+// Checking Hash Key Was not Changed
+$counter = strlen(LICENSE_KEY . $_SERVER['SERVER_NAME']);
+$hash = $_SERVER['SERVER_NAME'] . ',' . LICENSE_KEY . ',' . $counter;
+if (settings::get('site_configuration') !== $hash) {
+  echo 'License error';
+  exit();
+}
+// Checking License Key is Same
+if (settings::get('site_license') !== LICENSE_KEY) {
+  echo 'License error';
+  exit();
+}
+if (!empty($_GET['license'])) {
+  echo DB_DATABASE;
+}
