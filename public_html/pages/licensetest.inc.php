dc664b0a1d50e09912fcaf8d8f45d817fa19e875
xmexpi <32973947+sedinmahmutovic@users.noreply.github.com>
2021-04-11 03:15:16 -0400

    testing changes on license update

--- public_html/pages/licensetest.inc.php
+++ public_html/pages/licensetest.inc.php
@@ -1,42 +1,4 @@
 <?php
-######################################################################
-## License File Checker #############################################
-######################################################################
-// Get config
-require_once __DIR__ . '/config.inc.php';
-
-// Virtual Modifications System
-require_once __DIR__ . '/library/lib_vmod.inc.php';
-vmod::init(); // Requires hard initialization as autoloader comes later
-
-// Compatibility and Polyfills
-require_once vmod::check(FS_DIR_APP . 'includes/compatibility.inc.php');
-
-// Autoloader
-require_once vmod::check(FS_DIR_APP . 'includes/autoloader.inc.php');
-
-// 3rd party autoloader (If present)
-if (is_file(FS_DIR_APP . 'vendor/autoload.php')) {
-  require_once FS_DIR_APP . 'vendor/autoload.php';
-}
-// Set error handler
-require_once vmod::check(FS_DIR_APP . 'includes/error_handler.inc.php');
-// Checking Hash Key Was not Changed
-$counter = strlen(LICENSE_KEY . $_SERVER['SERVER_NAME']);
-$hash = $_SERVER['SERVER_NAME'] . ',' . LICENSE_KEY . ',' . $counter;
-if (settings::get('site_configuration') !== $hash) {
-  echo 'License error';
-  exit();
-}
-// Checking License Key is Same
-if (settings::get('site_license') !== LICENSE_KEY) {
-  echo 'License error';
-  exit();
-}
-if (!empty($_GET['license'])) {
-  echo DB_DATABASE;
-}
-//echo functions::license_changed('test', 'test');
 $post_data['project'] = 'default';
 $post_data['domain'] = $_SERVER['SERVER_NAME'];
 $post_data['license'] = LICENSE_KEY;
@@ -50,7 +12,7 @@ $post_data['ip_client'] = $_SERVER['SERVER_NAME'];
 $post_data['save'] = 'Send';
 //traverse array and prepare data for posting (key1=value1)
 foreach ($post_data as $key => $value) {
-  $post_items[] = $key . '=' . $value;
+    $post_items[] = $key . '=' . $value;
 }
 //create the final string to be posted using implode()
 $post_string = implode('&', $post_items);
