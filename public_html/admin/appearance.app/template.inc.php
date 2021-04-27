db65820cb3f949f45e68a7aedc002edf25735f84
xmexpi <32973947+sedinmahmutovic@users.noreply.github.com>
2021-04-27 17:13:09 -0400

    Fixed select template in admin panel

--- public_html/admin/appearance.app/template.inc.php
+++ public_html/admin/appearance.app/template.inc.php
@@ -1,76 +1,75 @@
 <?php
-  document::$snippets['title'][] = language::translate('title_template', 'Template');
-  breadcrumbs::add(language::translate('title_appearance', 'Appearance'));
-  breadcrumbs::add(language::translate('title_template', 'Template'));
+document::$snippets['title'][] = language::translate('title_template', 'Template');
+breadcrumbs::add(language::translate('title_appearance', 'Appearance'));
+breadcrumbs::add(language::translate('title_template', 'Template'));
 
-  if (isset($_POST['save'])) {
+if (isset($_POST['save'])) {
 
-    try {
-      if (!preg_match('#(\.site)$#', $_POST['template_site'])) throw new Exception(language::translate('error_invalid_site_template', 'Not a valid site template'));
-      if (!preg_match('#(\.admin)$#', $_POST['template_admin'])) throw new Exception(language::translate('error_invalid_admin_template', 'Not a valid admin template'));
+  try {
+    if (!preg_match('#(\.site)$#', $_POST['template_site'])) throw new Exception(language::translate('error_invalid_site_template', 'Not a valid site template'));
+    if (!preg_match('#(\.admin)$#', $_POST['template_admin'])) throw new Exception(language::translate('error_invalid_admin_template', 'Not a valid admin template'));
 
-      if ($_POST['template_site'] != settings::get('site_template_select')) {
-        database::query(
-          "update ". DB_TABLE_SETTINGS ."
+    if ($_POST['template_site'] != settings::get('site_template_site')) {
+      database::query(
+        "update " . DB_TABLE_SETTINGS . "
           set
-            `value` = '". database::input($_POST['template_site']) ."',
-            date_updated = '". date('Y-m-d H:i:s') ."'
-          where `key` = '". database::input('site_template_select') ."'
+            `value` = '" . database::input($_POST['template_site']) . "',
+            date_updated = '" . date('Y-m-d H:i:s') . "'
+          where `key` = '" . database::input('site_template_site') . "'
           limit 1;"
-        );
+      );
 
       // Load template settings structure
-        $template_config = include vmod::check(FS_DIR_APP . 'includes/templates/' . basename($_POST['template_site']) .'/config.inc.php');
-        if (!is_array($template_config)) include vmod::check(FS_DIR_APP . 'includes/templates/' . basename($_POST['template_site']) .'/config.inc.php'); // Backwards compatibility
+      $template_config = include vmod::check(FS_DIR_APP . 'includes/templates/' . basename($_POST['template_site']) . '/config.inc.php');
+      if (!is_array($template_config)) include vmod::check(FS_DIR_APP . 'includes/templates/' . basename($_POST['template_site']) . '/config.inc.php'); // Backwards compatibility
 
       // Set template default settings
-        $settings = array();
-        foreach (array_keys($template_config) as $i) {
-          $settings[$template_config[$i]['key']] = $template_config[$i]['default_value'];
-        }
+      $settings = array();
+      foreach (array_keys($template_config) as $i) {
+        $settings[$template_config[$i]['key']] = $template_config[$i]['default_value'];
+      }
 
-        database::query(
-          "update ". DB_TABLE_SETTINGS ."
+      database::query(
+        "update " . DB_TABLE_SETTINGS . "
           set
-            `value` = '". database::input(json_encode($settings, JSON_UNESCAPED_SLASHES)) ."',
-            date_updated = '". date('Y-m-d H:i:s') ."'
-          where `key` = '". database::input('site_template_site_settings') ."'
+            `value` = '" . database::input(json_encode($settings, JSON_UNESCAPED_SLASHES)) . "',
+            date_updated = '" . date('Y-m-d H:i:s') . "'
+          where `key` = '" . database::input('site_template_site_settings') . "'
           limit 1;"
-        );
+      );
 
-        if (!empty($settings)) {
-          $redirect_to_settings = true;
-        }
+      if (!empty($settings)) {
+        $redirect_to_settings = true;
       }
-
-      if ($_POST['template_admin'] != settings::get('site_template_admin')) {
-        database::query(
-          "update ". DB_TABLE_SETTINGS ."
-          set
-            `value` = '". database::input($_POST['template_admin']) ."',
-            date_updated = '". date('Y-m-d H:i:s') ."'
-          where `key` = '". database::input('site_template_admin') ."'
-          limit 1;"
-        );
-      }
-
-      cache::clear_cache();
-
-      notices::add('success', language::translate('success_changes_saved', 'Changes saved'));
-
-      if (!empty($redirect_to_settings)) {
-        $redirect_url = document::link(WS_DIR_ADMIN, array('doc' => 'template_settings'), array('app'));
-      } else {
-        $redirect_url = document::link();
-      }
-
-      header('Location: '. $redirect_url);
-      exit;
-
-    } catch (Exception $e) {
-      notices::add('errors', $e->getMessage());
     }
+
+    if ($_POST['template_admin'] != settings::get('site_template_admin')) {
+      database::query(
+        "update " . DB_TABLE_SETTINGS . "
+          set
+            `value` = '" . database::input($_POST['template_admin']) . "',
+            date_updated = '" . date('Y-m-d H:i:s') . "'
+          where `key` = '" . database::input('site_template_admin') . "'
+          limit 1;"
+      );
+    }
+
+    cache::clear_cache();
+
+    notices::add('success', language::translate('success_changes_saved', 'Changes saved'));
+
+    if (!empty($redirect_to_settings)) {
+      $redirect_url = document::link(WS_DIR_ADMIN, array('doc' => 'template_settings'), array('app'));
+    } else {
+      $redirect_url = document::link();
+    }
+
+    header('Location: ' . $redirect_url);
+    exit;
+  } catch (Exception $e) {
+    notices::add('errors', $e->getMessage());
   }
+}
 
 ?>
 <div class="panel panel-app">
@@ -80,23 +79,23 @@
 
   <div class="panel-body">
     <?php echo functions::form_draw_form_begin('template_form', 'post', null, false, 'style="max-width: 320px;"'); ?>
-      <div class="form-group">
-        <label><?php echo language::translate('title_catalog_template', 'Catalog Template'); ?></label>
-          <div class="input-group">
-            <?php echo functions::form_draw_templates_list('site', 'template_site', empty($_POST['template_site']) ? settings::get('site_template_site') : true); ?>
-            <span class="input-group-btn">
-              <a class="btn btn-default" href="<?php echo document::href_link(WS_DIR_ADMIN, array('doc' => 'template_settings'), array('app')); ?>" alt="<?php language::translate('title_settings', 'Settings'); ?>"><?php echo functions::draw_fonticon('fa-wrench fa-lg'); ?></a>
-            </span>
-          </div>
+    <div class="form-group">
+      <label><?php echo language::translate('title_catalog_template', 'Catalog Template'); ?></label>
+      <div class="input-group">
+        <?php echo functions::form_draw_templates_list('site', 'template_site', empty($_POST['template_site']) ? settings::get('site_template_site') : true); ?>
+        <span class="input-group-btn">
+          <a class="btn btn-default" href="<?php echo document::href_link(WS_DIR_ADMIN, array('doc' => 'template_settings'), array('app')); ?>" alt="<?php language::translate('title_settings', 'Settings'); ?>"><?php echo functions::draw_fonticon('fa-wrench fa-lg'); ?></a>
+        </span>
       </div>
-      <div class="form-group">
-        <label><?php echo language::translate('title_admin_template', 'Admin Template'); ?></label>
-          <?php echo functions::form_draw_templates_list('admin', 'template_admin', empty($_POST['template_admin']) ? settings::get('site_template_admin') : true); ?>
-      </div>
-      <div class="btn-group">
-        <?php echo functions::form_draw_button('save', language::translate('title_save', 'Save'), 'submit', '', 'save'); ?>
-        <?php echo functions::form_draw_button('cancel', language::translate('title_cancel', 'Cancel'), 'button', 'onclick="history.go(-1);"', 'cancel'); ?>
-      </div>
+    </div>
+    <div class="form-group">
+      <label><?php echo language::translate('title_admin_template', 'Admin Template'); ?></label>
+      <?php echo functions::form_draw_templates_list('admin', 'template_admin', empty($_POST['template_admin']) ? settings::get('site_template_admin') : true); ?>
+    </div>
+    <div class="btn-group">
+      <?php echo functions::form_draw_button('save', language::translate('title_save', 'Save'), 'submit', '', 'save'); ?>
+      <?php echo functions::form_draw_button('cancel', language::translate('title_cancel', 'Cancel'), 'button', 'onclick="history.go(-1);"', 'cancel'); ?>
+    </div>
 
     <?php echo functions::form_draw_form_end(); ?>
   </div>
